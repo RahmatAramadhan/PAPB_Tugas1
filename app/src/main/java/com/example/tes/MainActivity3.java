c27733c3c78188cd39bf9ce123ba771327567b09
rahmataramadhan <rahmataramadhan32@gmail.com>
2024-10-13 20:41:55 +0700

    feat: event handling login (main activity 3)

--- app/src/main/java/com/example/tes/MainActivity3.java
+++ app/src/main/java/com/example/tes/MainActivity3.java
@@ -1,6 +1,10 @@
 package com.example.tes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    Button btnLogin;
+    EditText etEmail;
+    EditText etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.layout1);
+        setContentView(R.layout.activity_main3);
+
+        btnLogin    = findViewById(R.id.btn_login);
+        etEmail     = findViewById(R.id.et_email);
+        etPassword  = findViewById(R.id.et_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email    = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(MainActivity3.this, "Email dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity3.this, "Berhasil Log in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
