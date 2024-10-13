5518580fa75de8987ffbfe5cee29419b418d23db
rahmataramadhan <rahmataramadhan32@gmail.com>
2024-10-13 21:11:18 +0700

    feat: event handling login (main activity 4)

--- app/src/main/java/com/example/tes/MainActivity4.java
+++ app/src/main/java/com/example/tes/MainActivity4.java
@@ -1,6 +1,10 @@
 package com.example.tes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    Button btnLogin;
+    TextView etEmail;
+    TextView etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        setContentView(R.layout.activity_main4);
+
+        btnLogin    = findViewById(R.id.btn_login);
+        etEmail     = findViewById(R.id.et_email);
+        etPassword  = findViewById(R.id.et_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email    = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(MainActivity4.this, "Email dan password tidak boleh kosong", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    Toast.makeText(MainActivity4.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
     }
 }
