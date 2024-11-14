c6ef55806d17c88570b59dd2fc1db1950073a1d9
rahmataramadhan <rahmataramadhan32@gmail.com>
2024-11-14 11:49:56 +0700

    feat: login with email password and with google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -36,6 +37,10 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.android.gms:play-services-auth:20.5.0")
+    implementation("com.google.firebase:firebase-auth:21.0.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/example/tes/MainActivity4.java
+++ app/src/main/java/com/example/tes/MainActivity4.java
@@ -5,29 +5,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import javax.annotation.Nullable;
+
 
 public class MainActivity4 extends AppCompatActivity {
 
+    static final int RC_SIGN_IN = 123;
     Button btnLogin;
+    ImageView btnGoogleSignIn;
     TextView etEmail;
     TextView etPassword;
+
+    FirebaseAuth auth;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main4);
 
-        btnLogin    = findViewById(R.id.btn_login);
-        etEmail     = findViewById(R.id.et_email);
-        etPassword  = findViewById(R.id.et_password);
+        btnGoogleSignIn = findViewById(R.id.btnGoogle);
+        btnLogin        = findViewById(R.id.btn_login);
+        etEmail         = findViewById(R.id.et_email);
+        etPassword      = findViewById(R.id.et_password);
+
+
+        auth        = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,12 +68,66 @@ public class MainActivity4 extends AppCompatActivity {
                     Toast.makeText(MainActivity4.this, "Email dan password tidak boleh kosong", Toast.LENGTH_SHORT).show();
 
                 }else{
-                    Toast.makeText(MainActivity4.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(MainActivity4.this, MrHead.class);
-                    startActivity(i);
+                    loginUser(email, password);
                 }
             }
         });
+        btnGoogleSignIn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                signInWithGoogle();
+            }
+        });
 
+    }
+
+    private void loginUser(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task ->{
+            if(task.isSuccessful()){
+                FirebaseUser user = auth.getCurrentUser();
+                Toast.makeText(MainActivity4.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(MainActivity4.this, MrHead.class);
+                startActivity(i);
+                finish();
+            }else{
+                Toast.makeText(MainActivity4.this, "Login gagal: "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void signInWithGoogle(){
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            try {
+                GoogleSignInAccount account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException.class);
+                if (account != null){
+                    firebaseAuthWithGoogle(account);
+                }
+            } catch (ApiException e){
+                Toast.makeText(this, "Google sign-in failed: "+ e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()){
+                        FirebaseUser user = auth.getCurrentUser();
+                        Toast.makeText(MainActivity4.this, "Login Berhasil dengan Google", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(MainActivity4.this, MrHead.class);
+                        startActivity(i);
+                        finish();
+                    }else{
+                        Toast.makeText(MainActivity4.this, "Autentikasi Gagal : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
